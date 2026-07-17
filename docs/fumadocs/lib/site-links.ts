e2ffc9ba6e8f413231a1ece6d7c12dd88eae040e
Jiadong Hou <149308795+iChubai@users.noreply.github.com>
2026-07-17 13:24:27 +0800

    Update WeChat QR code image path

--- docs/fumadocs/lib/site-links.ts
+++ docs/fumadocs/lib/site-links.ts
@@ -3,7 +3,7 @@ export const WORLDFOUNDRY_GITHUB_ISSUES = `${WORLDFOUNDRY_GITHUB_REPO}/issues`;
 export const WORLDFOUNDRY_GITHUB_DISCUSSIONS = `${WORLDFOUNDRY_GITHUB_REPO}/discussions`;
 export const WORLDFOUNDRY_SLACK_INVITE =
   'https://join.slack.com/t/worldfoundrycommunity/shared_invite/zt-43nbi9fw4-okYiELzZHp0_1UPa3dh3bQ';
-export const WORLDFOUNDRY_WECHAT_QR = '/wechat_qr.png';
+export const WORLDFOUNDRY_WECHAT_QR = '/wechat_qr_2.png';
 export const OPENENVISION_ORG = 'https://github.com/OpenEnvision';
 export const OPENENVISION_GAIA_REPO = 'https://github.com/OpenEnvision/Gaia';
 export const OPENENVISION_AWESOME_WORLD_MODELING =
